95af50b6532e50f5787f690823722b009c58aca7
Haile <hailecst@gmail.com>
2026-01-26 18:42:00 +0300

    hiding struct fields in the JSON object

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,11 +4,11 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
+	CreatedAt time.Time `json:"-"` // Use the - directive to hide it from json output
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`    // Movie release year
-	Runtime   int32     `json:"runtime"` // movie length in minutes
-	Geners    []string  `json:"geners"`  // slice of geners
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
+	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
+	Geners    []string  `json:"geners,omitempty"`  // Add the omitempty directive
 	Version   int32     `json:"version"`
 	// The version number starts at 1 and will be incremented each
 	// time the movie information is updated
