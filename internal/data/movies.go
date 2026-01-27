db2836d8eec2a84bef21cb68b98be20a568770a6
Haile <hailecst@gmail.com>
2026-01-27 18:04:18 +0300

    advanced json customization
    adding strings to int struct field

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,10 +6,14 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` // Use the - directive to hide it from json output
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
-	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
-	Geners    []string  `json:"geners,omitempty"`  // Add the omitempty directive
-	Version   int32     `json:"version"`
+	Year      int32     `json:"year,omitempty"` // Add the omitempty directive
+	// Use the Runtime type instead of int 32. Note that the omitempty directive will
+	// still work on this: if the Runtime field has the underlying value 0, then it will
+	// be considered empty and omitted -- and the MarshalJSON() method we jus made
+	// won't be called at all.
+	Runtime Runtime  `json:"runtime,omitempty"` // Add the omitempty directive
+	Geners  []string `json:"geners,omitempty"`  // Add the omitempty directive
+	Version int32    `json:"version"`
 	// The version number starts at 1 and will be incremented each
 	// time the movie information is updated
 }
