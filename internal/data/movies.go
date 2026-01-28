4c8eb96ec5353491eaf3d7aa6d65218be3ab0b00
Haile <hailecst@gmail.com>
2026-01-28 20:13:22 +0300

    creating a new movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/High-la/greenlight/internal/validator"
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -45,9 +46,29 @@ type MovieModel struct {
 }
 
 // Add a placeholder method for inserting a new record in the movies table.
+
+// The Insert() method accepts a pointer to a movie struct, which should contain
+// the data for the new record.
 func (m MovieModel) Insert(movie *Movie) error {
 
-	return nil
+	query := `
+		INSERT INTO movies 
+			(title, year, runtime, genres)
+		VALUES 
+			($1, $2, $3, $4)
+		RETURNING 
+			id, created_at, version`
+
+	// Create an args slice containing the values for the placeholder paras from
+	// the movie struct. Declaring this slice immediately next to our SQL query helps to
+	// make it nice and clear *what values are bring used where* in the query
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// Use the QueryRow() method to execute the SQL query on our connection pool,
+	// passing in the args slice as a variadic para and scanning the system
+	// generated id, created_at and version values into the movie struct.
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+
 }
 
 // Add a placeholder method for fetching a specific record from the movies table.
